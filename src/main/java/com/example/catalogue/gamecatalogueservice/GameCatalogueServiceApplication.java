c3208e457f51f9d8245eed05becd3a5a8f4540ac
lexash707 <lexashart@gmail.com>
2024-01-19 15:55:17 +0100

    initial commit

---
+++ src/main/java/com/example/catalogue/gamecatalogueservice/GameCatalogueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalogue.gamecatalogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCatalogueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameCatalogueServiceApplication.class, args);
+    }
+
+}
