0410ad9b62b83a60c236fcdcfa6e0430d7692945
lexash707 <lexashart@gmail.com>
2024-01-19 16:19:46 +0100

    simple get method

---
+++ src/main/java/com/example/catalogue/gamecatalogueservice/controller/CatalogueController.java
@@ -0,0 +1,26 @@
+package com.example.catalogue.gamecatalogueservice.controller;
+
+import com.example.catalogue.gamecatalogueservice.dto.Game;
+import com.example.catalogue.gamecatalogueservice.proxy.GameProxy;
+import com.example.catalogue.gamecatalogueservice.proxy.ReviewProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CatalogueController {
+
+    @Autowired
+    private GameProxy gameProxy;
+
+    @Autowired
+    private ReviewProxy reviewProxy;
+    @GetMapping("/get-games")
+    public List<Game> allGames(){
+        return gameProxy.allGames();
+    }
+
+
+}
