2bfdb6d4c4c1812ac12ab4ea987e4d18e77f2760
lexash707 <lexashart@gmail.com>
2024-01-19 16:10:28 +0100

    missclicked class instead of interface

--- src/main/java/com/example/catalogue/gamecatalogueservice/proxy/GameProxy.java
+++
@@ -1,8 +0,0 @@
-package com.example.catalogue.gamecatalogueservice.proxy;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(url = "localhost:8080", name = "game-service")
-public class GameProxy {
-
-}
--- src/main/java/com/example/catalogue/gamecatalogueservice/proxy/ReviewProxy.java
+++
@@ -1,7 +0,0 @@
-package com.example.catalogue.gamecatalogueservice.proxy;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(url = "localhost:8081", name = "game-review-service")
-public class ReviewProxy {
-}
