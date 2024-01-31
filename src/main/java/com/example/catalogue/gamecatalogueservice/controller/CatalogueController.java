029e43af98218aec47f2fe2f05c25e2410d8288f
lexash707 <lexashart@gmail.com>
2024-01-31 11:02:14 +0100

    add calls to other services

--- src/main/java/com/example/catalogue/gamecatalogueservice/controller/CatalogueController.java
+++ src/main/java/com/example/catalogue/gamecatalogueservice/controller/CatalogueController.java
@@ -1,11 +1,13 @@
 package com.example.catalogue.gamecatalogueservice.controller;
 
 import com.example.catalogue.gamecatalogueservice.dto.Game;
+import com.example.catalogue.gamecatalogueservice.dto.Review;
 import com.example.catalogue.gamecatalogueservice.proxy.GameProxy;
 import com.example.catalogue.gamecatalogueservice.proxy.ReviewProxy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +19,18 @@ public class CatalogueController {
 
     @Autowired
     private ReviewProxy reviewProxy;
+
     @GetMapping("/get-games")
     public List<Game> allGames() {
         return gameProxy.allGames();
+    }
+    @GetMapping("/get-game-review/{gameId}")
+    private List<Review> gameReviwe(@PathVariable Long gameId){
+        return reviewProxy.allReviewsOfGame(gameId);
+    }
+
+    @PostMapping("/review-game")
+    public ResponseEntity<HttpStatus> reviewGame(@RequestBody Review review){
+        return reviewProxy.reviewGame(review);
     }
 }
--- src/main/java/com/example/catalogue/gamecatalogueservice/proxy/ReviewProxy.java
+++ src/main/java/com/example/catalogue/gamecatalogueservice/proxy/ReviewProxy.java
@@ -2,8 +2,12 @@ package com.example.catalogue.gamecatalogueservice.proxy;
 
 import com.example.catalogue.gamecatalogueservice.dto.Review;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -11,5 +15,8 @@ import java.util.List;
 public interface ReviewProxy {
     @GetMapping("/game/{gameID}/reviews")
     List<Review> allReviewsOfGame(@PathVariable Long gameID);
+
+    @PostMapping("/game/reviews")
+    ResponseEntity<HttpStatus> reviewGame(@RequestBody Review review);
 
 }
